afedbf3416e2cc681c61dbe7f0d7ebb6e620c738
Johan Peraldi <johan.peraldi@gmail.com>
2023-05-29 21:30:04 +0300

    Replace profile picture and remove unnecessary files

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
-const name = 'Jean Bon';
+const name = 'Johan';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/profile.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
